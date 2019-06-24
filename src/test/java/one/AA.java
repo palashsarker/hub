c1e4819213f280b4192efcdbd5238df8f5678ddf
palsa <palsarker@gmail.com>
2019-06-24 19:33:40 -0400

    001

---
+++ src/test/java/one/AA.java
@@ -0,0 +1,12 @@
+package one;
+
+import org.testng.annotations.Test;
+
+public class AA {
+	@Test
+	public void say()
+	{
+		System.out.println("==============ppp");
+	}
+
+}
---
+++ src/test/java/one/BB.java
@@ -0,0 +1,12 @@
+package one;
+
+import org.testng.annotations.Test;
+
+public class BB {
+	@Test
+	public void info()
+	{
+		System.out.println("======in");
+	}
+
+}
